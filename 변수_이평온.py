11022cee8e2de8d152414eaf56a595af9aa764ff
leepyengon <131439090+leepyengon@users.noreply.github.com>
2023-05-12 16:58:52 +0900

    Add files via upload

---
+++ 변수_이평온.py
@@ -0,0 +1,14 @@
+#2023.05.12
+#내가 희망하는 키 출력
+
+name="파이썬"
+age=20
+print("제이름은 %s입니다.\n 나이는 $d."%(name,age,height)
+      
+hope=int(input("희망하는 키를 작성해라:"))
+print("제이름은 %입니다.\n나이는 %d,희망키는 %d ."%(name,age,hope))
+print("제이름은 %{}입니다.\n나이는 {},희망키는 {}.".format(name,age,hope))
+
+tmp=hope=height
+
+print("그럼 앞으로 %d 만큼 커야겠네요ㅋㄷ" %(hope-height))
